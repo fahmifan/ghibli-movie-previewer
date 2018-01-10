0bc45cfb8251f4d87842bcf6643641a2f51be25a
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-10 18:11:30 +0700

    made to render Home container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
+import Home from './containers/Home/Home';
 
 class App extends Component {
   render() {
     return (
-      <header className='tc pv4 pv5-ns'>
-        <img src={logo} className='br-100 pa1 ba b--black-10 h3 w3' alt='avatar' />
-        <h1 className='f5 f4-ns fw6 mid-gray'>Jasper Whitehouse</h1>
-        <h2 className='f6 gray fw2 ttu tracked'>Los Angeles</h2>
-      </header>
+      <Home />
     );
   }
 }
