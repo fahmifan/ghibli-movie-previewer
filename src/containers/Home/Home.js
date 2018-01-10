1b26f289a8f3175c21e5353a08ee9b37ccbc6c93
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-10 21:09:32 +0700

    Add new state

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,6 +5,10 @@ import axios from 'axios';
 import MovieCard from '../../components/MovieCard/MovieCard';
 
 class Home extends Component {
+  state = {
+    films: null,
+    loading: false
+  }
   render() {
     return (
       <div className="w-90 w-100-ns pt5 mh0 center">
